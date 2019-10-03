df833d6f9c4c51ecc9f374e3248d6333834f6915
rramaker <rramaker@Rynes-MacBook-Pro.local>
2019-10-03 13:52:07 -0500

    added multi-group functionality

--- R/RBDA.R
+++ R/RBDA.R
@@ -15,19 +15,17 @@
 #'colData<-data.frame(testVariable=as.factor(c(rep(0,5),rep(1,5))), row.names=colnames(BRCA_Counts))
 #'RBDA(countData = BRCA_Counts,colData,testVariable = "testVariable", randomSeed = 1990, numCores=1)
 
-
 RBDA<-function(countData, colData, testVariable, batch_family_variable=NULL, randomSeed=1990, minP=0.000005, numCores=4){
   set.seed(randomSeed)
   countRanks<-apply(countData, 2, function(x) rank(-x, ties.method = "random"))
   countRanks<-apply(countRanks,2,as.numeric)
-  countVar1<-apply(countRanks[,which(colData[,testVariable]==levels(as.factor(colData[,testVariable]))[1])],1,function(x) sum(abs(x-mean(x))^2))
   if(length(which(colData[,testVariable]==levels(as.factor(colData[,testVariable]))[2]))==1){
-    countVar2<-0
+    countVar_list<-list(apply(countRanks[,which(colData[,testVariable]==levels(as.factor(colData[,testVariable]))[1])],1,function(x) sum(abs(x-mean(x))^2)),0)
   }
   if(length(which(colData[,testVariable]==levels(as.factor(colData[,testVariable]))[2]))>1){
-    countVar2<-apply(countRanks[,which(colData[,testVariable]==levels(as.factor(colData[,testVariable]))[2])],1,function(x) sum(abs(x-mean(x))^2))
+    countVar_list<-lapply(1:length(levels(as.factor(colData[,testVariable]))), function(var) apply(countRanks[,which(colData[,testVariable]==levels(as.factor(colData[,testVariable]))[var])],1,function(x) sum(abs(x-mean(x))^2)))
   }
-  totVar<-apply(countRanks,1,function(x) sum(abs(x-mean(x))^2))/(countVar1+countVar2)
+  totVar<-apply(countRanks,1,function(x) sum(abs(x-mean(x))^2))/apply(do.call(rbind, countVar_list), 2, sum)
   totVar[which(totVar=="Inf")]<-NA
 
   samp_func<-function(countRanks, seed){
@@ -59,7 +57,7 @@ RBDA<-function(countData, colData, testVariable, batch_family_variable=NULL, ran
     Samp_totVar_tot<-unlist(parallel::mclapply(1:ceiling((1/minP)/nrow(countData)), function(x) samp_func(countRanks, x),mc.cores = numCores))
     batch_fam_Ps<-((rank(c(-max_batch_fam_statistic,-Samp_totVar_tot),ties.method="max")[1:length(max_batch_fam_statistic)]) - rank(-max_batch_fam_statistic,ties.method="max"))/length(Samp_totVar_tot)
     Ps<-((rank(c(-totVar,-Samp_totVar_tot),ties.method="max")[1:length(totVar)]) - rank(-totVar,ties.method="max"))/length(Samp_totVar_tot)
-    resultFrame<-do.call(cbind,list((rank(countVar1,ties.method = "max")/length(countVar1))*100,totVar,Ps,batch_fam_Ps))
+    resultFrame<-do.call(cbind,list((rank(countVar_list[[which.max(table(colData[,1]))]],ties.method = "max")/length(countVar_list[[which.max(table(colData[,1]))]]))*100,totVar,Ps,batch_fam_Ps))
     colnames(resultFrame)<-c("variance_rank", "test_statistic", "p_value","min_batch_fam_p_value")
     row.names(resultFrame)<-row.names(countData)
     return(resultFrame)
@@ -68,7 +66,7 @@ RBDA<-function(countData, colData, testVariable, batch_family_variable=NULL, ran
   if(is.null(batch_family_variable)){
     Samp_totVar_tot<-unlist(parallel::mclapply(1:ceiling((1/minP)/nrow(countData)), function(x) samp_func(countRanks, x),mc.cores = numCores))
     Ps<-((rank(c(-totVar,-Samp_totVar_tot),ties.method="max")[1:length(totVar)]) - rank(-totVar,ties.method="max"))/length(Samp_totVar_tot)
-    resultFrame<-do.call(cbind,list((rank(countVar1,ties.method = "max")/length(countVar1))*100,totVar,Ps))
+    resultFrame<-do.call(cbind,list((rank(countVar_list[[which.max(table(colData[,1]))]],ties.method = "max")/length(countVar_list[[which.max(table(colData[,1]))]]))*100,totVar,Ps))
     colnames(resultFrame)<-c("variance_rank", "test_statistic", "p_value")
     row.names(resultFrame)<-row.names(countData)
     return(resultFrame)
